d08f65d8c7529e9b0501fc4c985819e946e46f87
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-07 13:11:32 +0700

    penambahan halaman sign up

---
+++ lib/screen/signup_screen.dart
@@ -0,0 +1,109 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/countries.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:uangku/theme.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      bottom: false,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Color(0xff45B8F0),
+                  Color(0xff2E36A3),
+                ])),
+            child: Image.asset(
+              'assets/image1.png',
+              width: MediaQuery.of(context).size.width,
+              height: 450,
+              fit: BoxFit.cover,
+            ),
+          ),
+          ListView(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20)),
+                    color: whiteColor),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Text(
+                      'Sign Up',
+                      style: regularTextStyle.copyWith(fontSize: 20),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Choose your country code and\nenter your phone number',
+                      style: regulersoftTextStyle.copyWith(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: edge),
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: edge),
+                      child: IntlPhoneField(
+                        decoration: InputDecoration(labelText: 'Phone Number'),
+                        onChanged: (phone) {
+                          print(phone.completeNumber);
+                        },
+                        onCountryChanged: (country) {
+                          print('Country code changed to: ' + country.name);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      width: 335,
+                      height: 60,
+                      child: RaisedButton(
+                        onPressed: () {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => VerificationScreen()));
+                        },
+                        color: blueColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Text('Send Verification Code',
+                            style:
+                                regularwhiteTextStyle.copyWith(fontSize: 16)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:uangku/screen/signup_screen.dart';
 import 'package:uangku/theme.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,10 +11,38 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/bg.png'), fit: BoxFit.cover))),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/bg.png'), fit: BoxFit.cover)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 600),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: edge),
+                  child: Container(
+                    width: 335,
+                    height: 60,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpScreen()));
+                      },
+                      color: redColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Text(
+                        'Continue',
+                        style: whiteTextStyle.copyWith(fontSize: 18),
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
